0285635fa90aefca69d0a2982cdb9f556c09ba46
John Nguyen <johnn.mail@icloud.com>
2016-02-23 17:04:23 -0600

    commit

---
+++ ecomMVC1/app/controllers/home.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JohnMM2014
+ * Date: 2/23/16
+ * Time: 3:44 PM
+ */
+
+class Home extends Controller {
+    public function index ()
+    {
+        echo 'home/index';
+    }
+}
--- ecomMVC1/app/core/app.php
+++ ecomMVC1/app/core/app.php
@@ -9,10 +9,21 @@
 
 class App
 {
+
+    protected $controller = 'home';
+
+    protected $method = 'index';
+
+    protected $params = [];
+
     public function _construct()
     {
-        echo 'OK!';
+        $url = $this->parseUrl();
+    }
+
+    public function parseUrl() {
+        if(isset($_GET['url'])) {
+            return $url = filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL);
+        }
     }
 }
-
-echo "";
